7459e615105e83234b4949f7334340bd72ea68b6
Chris Walsh <chris@offwhitehouse.co.uk>
2022-08-22 09:03:18 +0100

    Day 24 part a

---
+++ 23_b.rs
@@ -0,0 +1,626 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+use std::process;
+
+static mut low_score: i64 = 999999999;
+static mut low_history: Vec<String> = Vec::new();
+static mut all_history: Vec::<(String, i32)> = Vec::new();
+
+
+
+
+
+
+
+
+fn main() -> io::Result<()> {
+  let mut map_in = Vec::new();
+
+  map_in.push("#############");
+  map_in.push("#.. . . . ..#");
+  map_in.push("###.#.#.#.###");
+  map_in.push("###.#.#.#.###");
+  map_in.push("###.#.#.#.###");
+  map_in.push("###.#.#.#.###");
+  map_in.push("#############");
+
+  let mut max_x = 0;
+  let mut max_y = 0;
+
+  let mut map_array = Vec::new();
+
+
+  for line in map_in {
+    max_y += 1;
+    if line.len() > max_x {
+      max_x = line.len();
+    }
+    let mut map_line = Vec::new();
+    for c in line.chars() {
+      map_line.push(c);
+    }
+    map_array.push(map_line.clone());
+  }
+
+  let mut spaces = Vec::new();
+
+  for y in 0..max_y {
+    for x in 0..max_x {
+      if map_array[y][x] == '.' {
+        let mut pair=Vec::new();
+        pair.push(y as i32);
+        pair.push(x as i32);
+        spaces.push(pair.clone());
+      }
+    }
+  }
+
+  println!("SPACES: {:?}", spaces.clone());
+ 
+      
+
+  //println!("{:?}", map_array);
+
+  let mut empty_map = Vec::new();
+  let mut zeroes = Vec::new();
+
+  for _ in 0..15 {
+    zeroes.push(-9);
+  }
+  for _ in 0..15 {
+    empty_map.push(zeroes.clone());
+  }
+
+  for y in 0..max_y {
+    for x in 0..max_x {
+      if map_array[y][x] == '.' || map_array[y][x] == ' ' {
+        empty_map[y as usize][x as usize] = -1;
+      }
+    }
+  }
+
+
+  // Calculate distances and paths
+  
+  let mut map = Vec::new();
+  let mut steps = Vec::new();
+  let mut step = Vec::new();
+
+  let mut paths = Vec::new();
+  let mut path = Vec::new();
+  let mut dummy_path = Vec::new();
+  dummy_path.push(0);
+  dummy_path.push(0);
+  dummy_path.push(0);
+
+
+
+  for _ in 0..23 {
+    step.push(0);
+    path.push(dummy_path.clone());
+  }
+
+  for _ in 0..23 {
+    steps.push(step.clone());
+    paths.push(path.clone());
+  }
+
+
+  for n in 0..spaces.len() {
+    println!(" == {} ======== ", n);
+    let mut this_map = empty_map.clone();
+    this_map[spaces[n][0] as usize][spaces[n][1] as usize] = 0;
+    let mut keep_going = 1;
+    while keep_going == 1 {
+      keep_going = 0;
+      for y in 1..max_y-1 {
+        for x in 1..max_x-1 {
+          //println!("  - x: {} ; y: {}", x, y);
+          for foo in this_map.clone() {
+            //println!("  mapline::: {:?}", foo);
+          }
+          if this_map[y][x]  == -1 {keep_going = 1;}
+          if this_map[y][x] >=0 && this_map[y-1][x] == -1 {this_map[y-1][x] = this_map[y][x] + 1;}
+          if this_map[y][x] >=0 && this_map[y+1][x] == -1 {this_map[y+1][x] = this_map[y][x] + 1;}
+          if this_map[y][x] >=0 && this_map[y][x-1] == -1 {this_map[y][x-1] = this_map[y][x] + 1;}
+          if this_map[y][x] >=0 && this_map[y][x+1] == -1 {this_map[y][x+1] = this_map[y][x] + 1;}
+        }
+      }
+    }
+    for z in 0..spaces.len() {
+      steps[n][z] = this_map[spaces[z][0] as usize][spaces[z][1] as usize];  
+      steps[z][n] = this_map[spaces[z][0] as usize][spaces[z][1] as usize]; 
+    }
+  }
+
+  println!("FINISHED WITH SPACES");
+  // CALCULATE PATHS
+  for from in 0..spaces.len() {
+    for to in 0..spaces.len() {
+      let mut path = Vec::new();
+
+      let from_x = spaces[from][1];
+      let from_y = spaces[from][0];
+      let to_x = spaces[to][1];
+      let to_y = spaces[to][0];
+
+      // Up
+      if from_y == 3 {
+        path.push(coord_to_space(from_x as i32, (from_y-1) as i32, spaces.clone()));
+      }
+      if from_y == 4 {
+        path.push(coord_to_space(from_x as i32, (from_y-1) as i32, spaces.clone()));
+        path.push(coord_to_space(from_x as i32, (from_y-2) as i32, spaces.clone()));
+      }
+      if from_y == 5 {
+        path.push(coord_to_space(from_x as i32, (from_y-1) as i32, spaces.clone()));
+        path.push(coord_to_space(from_x as i32, (from_y-2) as i32, spaces.clone()));
+        path.push(coord_to_space(from_x as i32, (from_y-3) as i32, spaces.clone()));
+      }
+
+
+      // along corridor
+      if from_x < to_x {
+        for x in from_x+1..to_x+1 {
+          //println!("Moving along corridor: {}", x);
+          let s = coord_to_space(x as i32, 1, spaces.clone());
+          if s > -1 {
+            path.push(s);
+          }
+        }
+      } 
+      if from_x > to_x {
+        for x in to_x..from_x {
+          //println!("Moving along corridor: {}", x);
+          let s = coord_to_space(x as i32, 1, spaces.clone());
+          if s > -1 {
+            path.push(s);
+          }
+        }
+      } 
+
+      // Down
+      if to_y == 5 {
+        path.push(coord_to_space(to_x as i32, (to_y-3) as i32, spaces.clone()));
+        path.push(coord_to_space(to_x as i32, (to_y-2) as i32, spaces.clone()));
+        path.push(coord_to_space(to_x as i32, (to_y-1) as i32, spaces.clone()));
+        path.push(coord_to_space(to_x as i32, (to_y) as i32, spaces.clone()));
+      }
+      if to_y == 4 {
+        path.push(coord_to_space(to_x as i32, (to_y-2) as i32, spaces.clone()));
+        path.push(coord_to_space(to_x as i32, (to_y-1) as i32, spaces.clone()));
+        path.push(coord_to_space(to_x as i32, (to_y) as i32, spaces.clone()));
+      }
+
+      if to_y == 3 {
+        path.push(coord_to_space(to_x as i32, (to_y-1) as i32, spaces.clone()));
+        path.push(coord_to_space(to_x as i32, (to_y) as i32, spaces.clone()));
+      }
+      if to_y == 2 {
+        path.push(coord_to_space(to_x as i32, (to_y) as i32, spaces.clone()));
+      }
+      if from_x == to_x {
+        path = Vec::new();
+      }
+
+ 
+
+      println!("Path from {} to {} :: {:?}", from, to, path);
+      paths[from][to] = path.clone();
+
+    }
+  }
+
+
+
+
+  
+
+      
+
+
+
+ 
+
+  // PRINT STEPS MATRIX
+  println!("STEPS");
+  for x in 0..23 {
+    for y in 0..23 {
+      println!("Steps from {} to {}   =  {}", x, y, steps[x][y]);
+    }
+    println!("");
+  }
+
+  // PRINT PATHS MATRIX
+  for x in 0..23 {
+    for y in 0..23 {
+      println!("{:?}", paths[x][y]);
+    }
+  }
+
+
+
+
+
+
+  let mut history = Vec::new();
+  let mut score = 0;
+
+  for _ in 0..7 {
+    map.push('.');
+  }
+  // SET MAP
+  //map.push('B');
+  //map.push('C');
+  //map.push('B');
+  //map.push('D');
+
+  map.push('D');
+  map.push('D');
+  map.push('B');
+  map.push('A');
+
+  map.push('D');
+  map.push('C');
+  map.push('B');
+  map.push('A');
+  map.push('D');
+  map.push('B');
+  map.push('A');
+  map.push('C');
+
+  //map.push('A');
+  //map.push('D');
+  //map.push('C');
+  //map.push('A');
+
+  //map.push('D');
+  //map.push('D');
+  //map.push('B');
+  //map.push('A');
+  map.push('B');
+  map.push('C');
+  map.push('A');
+  map.push('C');
+
+
+
+
+
+  println!("Make first move...");
+  let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+  //println!("SCORE: {}", score);
+  //println!("MAP: {:?}", map.clone());
+  //println!("MAP: {:?}", print_map(map.clone()));
+
+  unsafe {println!("LOWEST SCORE: {}", low_score); for z in low_history.clone() { println!("  {:?}", z); }}
+
+  Ok(())
+}
+
+
+
+fn we_have_seen(mut thing: String, my_score: i32) -> bool {
+  let mut seen = false;
+  unsafe {
+  for x in all_history.clone() {
+    if x.0 == thing {
+      if my_score >= x.1 {
+        seen = true;
+        //println!("Seen, with a lower score"); 
+      } else {
+        //println!("Seen, with a higher score");
+      }
+    } else {
+    }
+  }
+  }
+  return seen
+}
+
+fn path_is_clear(mut from: i32, mut to: i32, mut map: Vec<char>, mut paths: Vec<Vec<Vec<i32>>>) -> bool {
+  let mut return_val = true;
+  for step in paths[from as usize][to as usize].clone() {
+    if map[step as usize] != '.' {
+      return_val = false;
+    }
+  }
+  return return_val;
+}
+
+fn make_move(paths: Vec<Vec<Vec<i32>>>, mut score: i32, mut history: Vec<String>, mut map: Vec<char>, steps: Vec<Vec<i32>>) -> (i32, Vec<String>, Vec<char>)  {
+  // Check to see if we're done
+  unsafe {
+  if (score as i64) >= low_score {
+    return (score, history.clone(), map.clone())
+  }
+  }
+
+
+  let map_string = print_map(map.clone());
+  //println!("Making move: {} : {}", map_string.clone(), score);
+
+  
+
+  if map_string == ".......+AAAA+BBBB+CCCC+DDDD" {
+    //println!("DONE! With score {} - {:?} {:?}", score, history.clone(), map_string);
+    for x in history.clone() {
+      //println!("   {:?}", x);
+    }
+    unsafe {if (score as i64) < low_score {
+      println!("NEW LOW: {}", score);
+      low_score = score as i64;
+      history.push(map_string);
+      unsafe {
+        for t in history.clone() {
+          println!("  {:?}", t);
+        }
+      }
+      low_history = history.clone();
+    } }
+
+      
+    return (score, history.clone(), map.clone())
+  }
+
+  // Check to see if we've already been here, otherwise add to history
+  unsafe {
+  if we_have_seen(map_string.clone(), score) {
+    //println!("already seen");
+    return (score, history.clone(), map.clone())
+  } else {
+    let pair = (map_string.clone(), score);
+    all_history.push(pair);
+    //all_history.push(map_string.clone());
+    history.push(map_string.clone());
+
+  }
+  }
+
+  for piece_num in 0..map.len() {
+  // Iterate over each piece
+  //   make each possible move
+  //     If piece in correct silo, lower berth, forget it: we're done
+    if (map[piece_num] == '.') {
+      continue;
+    }
+
+    if (map[piece_num] == 'A' && piece_num == 19 ) ||
+       (map[piece_num] == 'B' && piece_num == 20 ) ||
+       (map[piece_num] == 'C' && piece_num == 21 ) ||
+       (map[piece_num] == 'D' && piece_num == 22 ) {
+         continue;
+    }
+
+  //     If piece in correct silo, upper berth, and lower berth is OK, forget it: we're done
+    if (map[piece_num] == 'A' && piece_num == 15 && map[19] == 'A') ||
+       (map[piece_num] == 'B' && piece_num == 16 && map[20] == 'B') ||
+       (map[piece_num] == 'C' && piece_num == 17 && map[21] == 'C') ||
+       (map[piece_num] == 'D' && piece_num == 18 && map[22] == 'D') {
+         continue;
+    }
+
+    if (map[piece_num] == 'A' && piece_num == 11 && map[15] == 'A' && map[19] == 'A') ||
+       (map[piece_num] == 'B' && piece_num == 12 && map[16] == 'B' && map[20] == 'B') ||
+       (map[piece_num] == 'C' && piece_num == 13 && map[17] == 'C' && map[21] == 'C') ||
+       (map[piece_num] == 'D' && piece_num == 14 && map[18] == 'D' && map[22] == 'D') {
+         continue;
+    }    
+    if (map[piece_num] == 'A' && piece_num == 7 && map[11] == 'A' && map[15] == 'A' && map[19] == 'A') ||
+       (map[piece_num] == 'B' && piece_num == 8 && map[12] == 'B' && map[16] == 'B' && map[20] == 'B') ||
+       (map[piece_num] == 'C' && piece_num == 9 && map[13] == 'C' && map[17] == 'C' && map[21] == 'C') ||
+       (map[piece_num] == 'D' && piece_num == 10 && map[14] == 'D' && map[18] == 'D' && map[22] == 'D') {
+         continue;
+    }
+
+
+  let mut moved = 0;
+  //     If piece is anywhere, and correct silo lower berth is empty & accessible, move.
+    if (map[piece_num] == 'A' && map[19] == '.' && path_is_clear(piece_num as i32, 19, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[19] = 'A';
+      score += steps[piece_num][19];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'B' && map[20] == '.' && path_is_clear(piece_num as i32, 20, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[20] = 'B';
+      score += 10*steps[piece_num][20];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'C' && map[21] == '.' && path_is_clear(piece_num as i32, 21, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[21] = 'C';
+      score += 100*steps[piece_num][21];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'D' && map[22] == '.' && path_is_clear(piece_num as i32, 22, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[22] = 'D';
+      score += 1000*steps[piece_num][22];
+      //println!("piece is anywhere, and correct silo lower berth is empty & accessible");
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+
+  //     If piece is anywhere, and correct silo upper berth is E&A, and lower silo has correct occupant, move
+    if (map[piece_num] == 'A' && map[19] == 'A' && map[15] == '.' && path_is_clear(piece_num as i32, 15, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[15] = 'A';
+      score += steps[piece_num][15];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'B' && map[20] == 'B' && map[16] == '.' && path_is_clear(piece_num as i32, 16, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[16] = 'B';
+      score += 10*steps[piece_num][16];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'C' && map[21] == 'C' && map[17] == '.' && path_is_clear(piece_num as i32, 17, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[17] = 'C';
+      score += 100*steps[piece_num][17];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'D' && map[22] == 'D' && map[18] == '.' && path_is_clear(piece_num as i32, 18, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[18] = 'D';
+      score += 1000*steps[piece_num][18];
+      //println!("piece is anywhere, and correct silo upper berth is E&A, and lower silo has correct occupant");
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+
+
+
+    if (map[piece_num] == 'A' && map[19] == 'A' && map[15] == 'A' && map[11] == '.' && path_is_clear(piece_num as i32, 11, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[11] = 'A';
+      score += steps[piece_num][11];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'B' && map[20] == 'B' && map[16] == 'B' && map[12] == '.' && path_is_clear(piece_num as i32, 12, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[12] = 'B';
+      score += 10*steps[piece_num][12];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'C' && map[21] == 'C' && map[17] == 'C' && map[13] == '.' && path_is_clear(piece_num as i32, 13, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[13] = 'C';
+      score += 100*steps[piece_num][13];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'D' && map[22] == 'D' && map[18] == 'D' && map[14] == '.' && path_is_clear(piece_num as i32, 14, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[14] = 'D';
+      score += 1000*steps[piece_num][14];
+      //println!("piece is anywhere, and correct silo upper berth is E&A, and lower silo has correct occupant");
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+
+
+
+
+
+
+
+    if (map[piece_num] == 'A' && map[11] == 'A' && map[19] == 'A' && map[15] == 'A' && map[7] == '.' && path_is_clear(piece_num as i32, 7, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[7] = 'A';
+      score += steps[piece_num][7];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'B' && map[12] == 'B' && map[20] == 'B' && map[16] == 'B' && map[8] == '.' && path_is_clear(piece_num as i32, 8, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[8] = 'B';
+      score += 10*steps[piece_num][8];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'C' && map[13] == 'C' && map[21] == 'C' && map[17] == 'C' && map[9] == '.' && path_is_clear(piece_num as i32, 9, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[9] = 'C';
+      score += 100*steps[piece_num][9];
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+    if (map[piece_num] == 'D' && map[14] == 'D' && map[22] == 'D' && map[18] == 'D' && map[10] == '.' && path_is_clear(piece_num as i32, 10, map.clone(), paths.clone())) {
+      moved = 1;
+      map[piece_num] = '.';
+      map[10] = 'D';
+      score += 1000*steps[piece_num][10];
+      //println!("piece is anywhere, and correct silo upper berth is E&A, and lower silo has correct occupant");
+      let (score, history, map) = make_move(paths.clone(), score, history.clone(), map.clone(), steps.clone());
+    }
+
+
+
+
+
+
+
+  //     If piece is in silo, move to all available coridor spaces
+    if moved == 0 && piece_num > 6 {
+      for move_to in 0..7 {
+        if map[piece_num] != '.' && piece_num > 6 && map[move_to] == '.' && path_is_clear(piece_num as i32, move_to as i32, map.clone(), paths.clone()) {
+          let mut new_map = map.clone();
+          new_map[piece_num] = '.';
+          new_map[move_to] = map[piece_num];
+          let mut multiplier = 1;
+          if map[piece_num] == 'B' {multiplier = 10;}
+          if map[piece_num] == 'C' {multiplier = 100;}
+          if map[piece_num] == 'D' {multiplier = 1000;}
+
+          let add_steps = steps[piece_num][move_to] * multiplier;
+  unsafe {
+  if ((score+add_steps) as i64) >= low_score {
+    return (score, history.clone(), map.clone())
+  }
+  }
+
+          //println!("Moving from {} to {}", piece_num, move_to);
+          let (score, history, map) = make_move(paths.clone(), score+add_steps, history.clone(), new_map.clone(), steps.clone());
+        }
+      }
+    } 
+  }
+
+  // If no move possible, abort
+  (score, history.clone(), map.clone())
+}
+
+fn print_map(map: Vec<char>) -> String {
+  let mut val = String::from("");
+  for n in 0..7 {
+    val.push(map[n]);
+  }
+  val.push('+');
+  val.push(map[7]);
+  val.push(map[11]);
+  val.push(map[15]);
+  val.push(map[19]);
+  val.push('+');
+  val.push(map[8]);
+  val.push(map[12]);
+  val.push(map[16]);
+  val.push(map[20]);
+  val.push('+');
+  val.push(map[9]);
+  val.push(map[13]);
+  val.push(map[17]);
+  val.push(map[21]);
+  val.push('+');
+  val.push(map[10]);
+  val.push(map[14]);
+  val.push(map[18]);
+  val.push(map[22]);
+
+
+  (val.clone())
+}
+
+fn coord_to_space(x: i32, y: i32, spaces: Vec<Vec<i32>>) -> (i32) {
+  //println!("coord to space! x={}, y={}", x, y);
+  //println!("  {:?}", spaces);
+  for s in 0..spaces.len() {
+    //println!("Checking against x={}, y={}", spaces[s][1], spaces[s][0]);
+    if spaces[s][0] == y && spaces[s][1] == x {
+      //println!("coord x={} ; y={} is space {}", x, y, s);
+      return s as i32;
+    }
+  }
+  (-1)
+} 
+
---
+++ 24_a.rs
@@ -0,0 +1,190 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+use std::process;
+
+
+fn main() -> io::Result<()> {
+  let mut result_string = String::new();
+
+
+  let mut max_num = 1000000;
+
+  let mut regs = [0, 0, 0, 0];
+  // add x 10
+  let mut numbers1 = [10, 14, 14, -13, 10, -13, -7, 11, 10, 13, -4, -9, -13, -9];
+  // div z 1 or 26
+  let mut numbers2 = [1, 1, 1, 26, 1, 26, 26, 1, 1, 1, 26, 26, 26, 26];
+
+  // add y num
+  let mut numbers3 = [2, 13, 13, 9, 15, 3, 6, 5, 16, 1, 6, 3, 7, 9];
+
+
+
+  let mut z_results = Vec::new();
+  let mut z_empty: Vec::<i32> = Vec::new();
+  for i in 0..max_num+1 {
+    z_empty.push(0);
+  }
+  for i in 0..15 {
+    z_results.push(z_empty.clone());
+  }
+  z_results[14][0] = 1;
+
+  println!("Big array done");
+
+
+
+  for stage in (0..14).rev() { 
+    println!("stage: {}" , stage);
+
+
+  for input in 1..10 {
+   for input_z in 0..max_num {
+
+    let mut regs = [0, 0, 0, 0];
+    regs[3] = input_z;
+
+    let mut add_x = numbers1[stage];
+    let mut div_z = numbers2[stage];
+    let mut add_y = numbers3[stage];
+
+    //println!("{} {} {}", add_x, div_z, add_y);
+
+    // inp w
+    regs[0] = input;
+
+    // mul x 0
+    // add x z
+    regs[1] = regs[3];
+
+    // mod x 26
+    let mut sum = regs[1] % 26 ;
+    regs[1] = sum.clone();
+
+    // div z 1			<-- sometimes this number ; 1 or 26
+    let mut sum = regs[3] / div_z ;
+    regs[3] = sum.clone();
+
+    // add x 10    <-- number here
+    regs[1] += add_x;
+
+    // eql x w
+    // eql x 0
+    if regs[1] != regs[0] {regs[1]=1;} else {regs[1]=0;}
+
+    // mul y 0
+    // add y 25
+    // mul y x
+    // add y 1
+    regs[2] = ( regs[1] * 25 ) + 1;
+ 
+    // mul z y
+    regs[3] = regs[3] * regs[2];
+
+    // mul y 0
+    // add y w
+    // add y 2    <- number here
+    regs[2] = regs[0] + add_y;
+
+    // mul y x
+    regs[2] = regs[2] * regs[1];
+
+    // add z y
+    regs[3] += regs[2];
+
+    if regs[3].clone() <= max_num {
+    //println!("  checking stage+1: {} z: {} table: {}", stage+1, regs[3], z_results[stage+1][regs[3].clone() as usize]);
+
+    if z_results[stage+1][regs[3].clone() as usize] == 1 {
+      //println!("Stage {} : input {} with a previous z of {} produces a new z of {}", stage, input, input_z, regs[3]);
+      z_results[stage][input_z as usize] = 1;
+    }
+    }
+   }
+  }
+
+  }
+
+
+  // OK NOW TRY AND WORK OUT THE ANSWER
+  let mut input_z = 0;
+  for digit in 0..14 {
+    println!("Doing digit: {}", digit);
+
+    //println!("    Looking for the following: ");
+    //for i in 0..max_num+1 {
+    //  if z_results[digit+1][i as usize] == 1 {
+    //    println!("          {}", i);
+    //  }
+   // }
+
+    
+    for value in (1..10).rev() {
+      //println!("  Trying value: {}", value);
+
+      let mut regs = [0, 0, 0, 0];
+      regs[3] = input_z;
+
+      let mut add_x = numbers1[digit];
+      let mut div_z = numbers2[digit];
+      let mut add_y = numbers3[digit];
+
+
+      // inp w
+      regs[0] = value.clone();
+
+      // mul x 0
+      // add x z
+      regs[1] = regs[3];
+
+      // mod x 26
+      let mut sum = regs[1] % 26 ;
+      regs[1] = sum.clone();
+
+      // div z 1                  <-- sometimes this number ; 1 or 26
+      let mut sum = regs[3] / div_z ;
+      regs[3] = sum.clone();
+
+      // add x 10    <-- number here
+      regs[1] += add_x;
+
+      // eql x w
+      // eql x 0
+      if regs[1] != regs[0] {regs[1]=1;} else {regs[1]=0;}
+
+      // mul y 0
+      // add y 25
+      // mul y x
+      // add y 1
+      regs[2] = ( regs[1] * 25 ) + 1;
+
+      // mul z y
+      regs[3] = regs[3] * regs[2];
+
+      // mul y 0
+      // add y w
+      // add y 2    <- number here
+      regs[2] = regs[0] + add_y;
+
+      // mul y x
+      regs[2] = regs[2] * regs[1];
+
+      // add z y
+      regs[3] += regs[2];
+
+      //println!("     result z = {}", regs[3]);
+
+      if z_results[digit+1][regs[3].clone() as usize] == 1 {
+        //println!("GOTCHA - digit {} is {}", digit, value);
+        result_string += &value.to_string();
+        input_z = regs[3].clone();
+        break;
+      }
+
+   
+    }
+  }
+  println!("RESULT: {}", result_string);
+  Ok(())
+}
+
